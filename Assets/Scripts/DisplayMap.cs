54d0d3bc9fbc4215857beebfafac7b3f44577e7c
Su386yt <83100266+Su386yt@users.noreply.github.com>
2022-05-17 08:36:43 -0700

    Removed unecesesary functions

--- Assets/Scripts/DisplayMap.cs
+++ Assets/Scripts/DisplayMap.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drawing : MonoBehaviour {
+public class DisplayMap : MonoBehaviour {
     public GameObject pixel;
     public GameObject instantiatedPixel;
     public Pixel[,] world;
@@ -33,27 +33,5 @@ public class Drawing : MonoBehaviour {
             transform.position = new Vector3(-8.8f, transform.position.y - 0.1f / (float)scaleFactor, 0);
         }
         return;
-    }
-    public static double randdouble(double max) {
-        System.Random random = new System.Random();
-        return (max + .0000000001) * random.NextDouble();
-    }
-    public static int randint(int max) {
-        System.Random random = new System.Random();
-        return random.Next(max + 1);
-    }
-
-    static bool noDropRNG(double percentChance, int rolls) {
-        for (int x = rolls; x != 0; x--) {
-            System.Random random = new System.Random();
-
-            double randomNum = random.NextDouble();// Get a System.Random  value from 0.00 - 1.00
-            double adjustedPercentChance = percentChance;
-
-            if (randomNum <= adjustedPercentChance / 100) {
-                return true;
-            }
-        }
-        return false;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-        Debug.Log("this is a test");
+
     }
 
     // Update is called once per frame
