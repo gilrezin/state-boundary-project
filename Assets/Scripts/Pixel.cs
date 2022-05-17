ff90f8a0fc72a566855a92b6d2dccacb39bb7faa
Su386yt <83100266+Su386yt@users.noreply.github.com>
2022-05-17 12:31:18 -0700

    Added color

--- Assets/Scripts/Pixel.cs
+++ Assets/Scripts/Pixel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-public class  Pixel : MonoBehaviour {
+public class  Pixel {
     public double ElevationPercentage;
 
     public int[] Coordinate = new int[2];
@@ -30,10 +30,23 @@ public class  Pixel : MonoBehaviour {
 
     }
 
-    public void toggleSelect() {
+    public void ToggleSelect() {
         if (IsSelected)
             IsSelected = false;
         else
             IsSelected = true;
+    }
+
+    public Color32 GetLandColor() {
+        if (ElevationPercentage < .2)
+            return new Color32(116, 183, 49, 255);
+        else if (.2 <= ElevationPercentage && ElevationPercentage < .4)
+            return new Color32(78, 122, 36, 255);
+        else if (.4 <= ElevationPercentage && ElevationPercentage < .6)
+            return new Color32(96, 186, 100, 255);
+        else if (.6 <= ElevationPercentage && ElevationPercentage < .8)
+            return new Color32(96, 122, 100, 255);
+        else
+            return new Color32(145, 187, 150, 255);
     }
 }
